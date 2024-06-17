5daa06e6038ec0fe50e644f39acd899cbdfac635
Alba <albagar4@student.42malaga.com>
2024-06-17 16:28:07 +0200

    mal viaje parte 2

--- src/raycast/first_step.c
+++ src/raycast/first_step.c
@@ -15,10 +15,10 @@ t_coord	*first_step_x(t_data *data, t_tools *tools)
 	init->x = data->p_x;
 	init->y = data->p_y;
 	first_x->y = (int)(data->p_y);
-	if (data->p_a > 0 && data->p_a < M_PI)
+	if (data->angle > 0 && data->angle < M_PI)
 		first_x->y += 1;
 	distance = (fabs)(first_x->y - data->p_y);
-	first_x->x = distance / tan(data->p_a);
+	first_x->x = distance / tan(data->angle);
 	first_x->x = data->p_x + (first_x->x * tools->vector_y);
 	if (ft_delimiter(data, first_x) == 1)
 		return (init);
@@ -40,11 +40,11 @@ t_coord	*first_step_y(t_data *data, t_tools *tools)
 	init->x = data->p_x;
 	init->y = data->p_y;
 	first_y->x = (int)(data->p_x);
-	if (!(data->p_a > rad_convertor(90)
-			&& data->p_a < rad_convertor(270)))
+	if (!(data->angle > rad_convertor(90)
+			&& data->angle < rad_convertor(270)))
 		first_y->x += 1;
 	distance = (fabs)(first_y->x - data->p_x);
-	first_y->y = distance * tan(data->p_a);
+	first_y->y = distance * tan(data->angle);
 	first_y->y = data->p_y + (first_y->y * tools->vector_x);
 	if (ft_delimiter(data, first_y) == 1)
 		return (init);
