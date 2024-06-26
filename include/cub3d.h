a63a31c8de5830869bad9dc566facd75c9b2152d
Alba <albagar4@student.42malaga.com>
2024-06-26 15:55:33 +0200

    fixed : texture inversion

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: albagar4 <albagar4@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 17:20:53 by alvega-g          #+#    #+#             */
-/*   Updated: 2024/06/26 13:24:32 by albagar4         ###   ########.fr       */
+/*   Updated: 2024/06/26 15:52:04 by albagar4         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,6 +77,7 @@ typedef struct s_coord
 typedef struct s_tools
 {
 	mlx_texture_t	*orientation;
+	int				flag;
 	int				wall_size;
 	int				vector_x;
 	int				vector_y;
@@ -121,7 +122,8 @@ t_coord	*y_wall_finder(t_data *data, t_tools *tools);
 t_tools	*wall_distance(t_data *data);
 
 //ray_defines
-void	define_orientation(t_tools *tools, t_data *data);
+void	define_orientation_2(t_tools *tools, t_data *data);
+void	define_orientation_1(t_tools *tools, t_data *data);
 double	x_distance(t_data *data);
 double	y_distance(t_data *data);
 t_tools	*vector_define(t_data *data);
@@ -135,5 +137,5 @@ void	ray_loop(t_data *data);
 
 //texture.c
 int		ft_paint_txt(mlx_texture_t *tex, double draw_cross,
-			int y, int wall_size);
+			int y, t_tools *tools);
 #endif
